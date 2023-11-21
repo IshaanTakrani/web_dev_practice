b1cdb3d16c49a2cec2b4b9cb0489d449662da436
Ishaan Takrani <ishaantakrani@gmail.com>
2023-11-20 19:33:27 -0500

    Fixed number guess

--- number_guess/script.js
+++ number_guess/script.js
@@ -1,13 +1,42 @@
 
 // getting values from html document
-const guess = document.getElementById('in_text');
-const activator = document.getElementById('button'); 
-const output = document.getElementById('out1')
+let random_int = Math.random
+let guess = document.getElementById('in_text');
+let output = document.getElementById('out1')
+let activator = document.getElementById('button'); 
+let clear_button = document.getElementById('clear_button')
 
-// function that changes output paragraph to guess
-function myFunction(){
-    output.innerHTML = guess.value;
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// event handler:
-activator.addEventListener('click',myFunction)
+let randint = getRandomInt(1,10);
+let rand = randint.toString()
+
+function displayResult(){
+    console.log(rand);
+    console.log(guess.value);
+    if(guess.value == rand){
+        output.innerHTML = "Correct"
+    }
+    else{
+        output.innerHTML = "WRONG"
+    }  
+}
+
+
+// function displayResult(){
+//     output.innerHTML = "Hi"
+
+// }
+
+function clear(){
+    location.reload() 
+}
+
+// event handlers:
+activator.addEventListener('click',displayResult)
+clear_button.addEventListener('click',clear)
